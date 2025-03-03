36d936c52cac843d805d1e95c7364a5718dc49b2
dotsocialadmin <syed.rahman@dot.social>
2025-03-03 20:32:04 +0000

    test build

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Most minimal React application possible
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
